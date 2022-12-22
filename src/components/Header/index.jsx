65004feadaff0d092d9e4644f8d1a7b40ca31033
Ricardo <ricardoredes2006@gmail.com>
2022-12-22 15:27:35 -0300

    Create page detais e home

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,9 +1,21 @@
-import { Container } from "./styles";
+import { RiShutDownLine } from 'react-icons/ri'
+import { Container, Profile, Logout } from "./styles"
 
-export function Header(){
+export function Header() {
   return (
     <Container>
-      
+      <Profile>
+        <img src="https://github.com/ricardojcosta.png" alt="foto do usuário" />
+
+        <div>
+          <span>Bem-vindo</span>
+          <strong>Ricardo Costa</strong>
+        </div>
+      </Profile>
+
+      <Logout>
+        <RiShutDownLine />
+      </Logout>
     </Container>
-  );
+  )
 }
--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,50 @@
-import { Container } from './styles';
+import { Container, Links, Content } from "./styles"
 
-import { Header } from '../../components/Header';
-import { Button } from '../../components/Button';
+import { Header } from "../../components/Header"
+import { Button } from "../../components/Button"
+import { Section } from "../../components/Section"
+import { Tag } from "../../components/Tag"
+import { ButtonText } from "../../components/ButtonText"
 
-export function Details(){
+export function Details() {
   return (
     <Container>
-    
       <Header />
-      <Button title="Voltar" />
+
+      <main>
+        <Content>
+          <ButtonText title="Excluir nota" />
+
+          <h1> Introdução ao React</h1>
+          <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eos
+            inventore natus eius autem doloribus mollitia quis debitis
+            distinctio, dolorum non esse amet magni quisquam illo maiores quo
+            ipsa. Voluptatibus, atque? Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Nobis nulla aliquid accusamus eligendi dolores
+            repellendus esse cum deserunt id alias consequatur hic fuga
+            similique ad, aut voluptatibus doloremque exercitationem delectus.
+          </p>
+
+          <Section title="Links úteis">
+            <Links>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br</a>
+              </li>
+            </Links>
+          </Section>
+
+          <Section title="Marcadores">
+            <Tag title="express" />
+            <Tag title="node.js" />
+          </Section>
+
+          <Button title="Voltar" />
+        </Content>
+      </main>
     </Container>
   )
 }
