089f4f41bac07c37d4af0fa55bd66fb126d4a03a
Ricardo <ricardoredes2006@gmail.com>
2022-12-26 14:03:46 -0300

    Screen Edit User and New Notes

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from './styles/global';
 
 import theme from "./styles/theme"
 
-import { SignUp } from './pages/SignUp';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <SignUp />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
