a58209e7b0a1b9af7e28433064e7f28ced356635
Ricardo <ricardoredes2006@gmail.com>
2023-01-24 09:31:14 -0300

    user autentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { ThemeProvider } from 'styled-components'
-import GlobalStyle from './styles/global';
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { ThemeProvider } from "styled-components"
+import GlobalStyle from "./styles/global"
+
+import { AuthProvider } from "./hooks/auth"
 
 import theme from "./styles/theme"
 
-import { Routes } from './routes';
+import { Routes } from "./routes"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Routes />
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 )
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react';
 import { FiMail, FiLock } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 
+import { useAuth } from "../../hooks/auth";
 
 import { Input } from '../../components/Input';
 import { Button } from '../../components/Button';
@@ -8,33 +10,42 @@ import { Button } from '../../components/Button';
 import { Container, Form, Background } from "./styles";
 
 export function SignIn(){
-  return(
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const { signIn } = useAuth();
+
+  function handleSignIn(){
+    signIn({ email, password });
+  }
+
+  return (
     <Container>
       <Form>
         <h1>Rocket Notes</h1>
         <p>Aplicação para salvar e gerenciar seus links úteis</p>
 
         <h2>Faça seu login</h2>
-        <Input 
-        placeholder="Email"
-        type="text"
-        icon={FiMail}
+        <Input
+          placeholder="Email"
+          type="text"
+          icon={FiMail}
+          onChange={(e) => setEmail(e.target.value)}
         />
 
-        <Input 
-        placeholder="Senha"
-        type="password"
-        icon={FiLock}
+        <Input
+          placeholder="Senha"
+          type="password"
+          icon={FiLock}
+          onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Button title="Entrar"/>
-        <Link to="/register">
-          Criar Conta
-        </Link>
+        <Button title="Entrar" onClick={handleSignIn} />
+
+        <Link to="/register">Criar Conta</Link>
       </Form>
 
-      <Background/>
-
+      <Background />
     </Container>
-  );
+  )
 }
